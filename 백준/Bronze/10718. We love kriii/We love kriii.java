af666dced52e580937ca16afe411a5fe7439351f
intacka <70586428+intacka@users.noreply.github.com>
2022-08-31 20:58:45 +0900

    [Bronze V] Title: We love kriii, Time: 108 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		//두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+		
+		for(int i = 0;i < 2; ++i)
+			System.out.println("강한친구 대한육군");
+		
+	}
+
+}
