6eeaa01d6030d50876d9140907041312326c20a1
intacka <70586428+intacka@users.noreply.github.com>
2022-09-14 22:57:07 +0900

    [Bronze V] Title: 시험 성적, Time: 120 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		int a = Integer.parseInt(str);
+		
+		if(a >= 90) {
+			System.out.println("A");
+		} else if (a >= 80) {
+			System.out.println("B");
+		} else if (a >= 70) {
+			System.out.println("C");
+		} else if (a >= 60) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+		
+		
+		
+	}
+}
